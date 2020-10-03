2dd12a3c24cdf13facf25f4a52aeda3e94990009
Guilherme-Valle <guidovalle1608@gmail.com>
2020-10-03 17:16:25 -0300

    Adiciona botões do modal de order

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
@@ -15,6 +16,10 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
+            <Button btnType="Danger"
+                clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success"
+                clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
     );
 }
