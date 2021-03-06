1e2a6c11311c6330f561cbf5ba1bc12c9134b539
Guilherme-Valle <guidovalle1608@gmail.com>
2021-03-06 13:43:39 -0300

    Ajusta sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
 
-            {props.children}
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
 
-        </main>
-    </Aux>
-);
+    render() {
+        return (<Aux>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler} />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>)
+    }
+}
 
-export default layout;
+export default Layout;
