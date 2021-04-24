0b9d3087f320dddf9494917575835cfa3b7d98de
Guilherme-Valle <guidovalle1608@gmail.com>
2021-04-24 15:31:36 -0300

    Adiciona verificações para render

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import classes from './DrawerToggle.css'
 const drawerToggle = (props) => (
-    <div className={classes.DrawerToggle} 
-    onClick={props.clicked}>
+    <div className={classes.DrawerToggle}
+        onClick={props.clicked}>
         <div></div>
         <div></div>
         <div></div>
